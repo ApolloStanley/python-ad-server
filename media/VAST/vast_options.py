70f876b7b2bacfdc91e3d69c057aa5d744973f57
Matthew Edward Stanley <medwardstanley@roku.com>
2026-09-03 11:03:27 +0100

    added new media file with specific resolution

--- media/VAST/vast_options.py
+++ media/VAST/vast_options.py
@@ -47,9 +47,11 @@ def vast_stream1_custom(IP, PORT):
 <Tracking event="thirdQuartile"><![CDATA[http://{IP}:{PORT}/?thirdQuartile&stream=1]]></Tracking>
 <Tracking event="complete"><![CDATA[http://{IP}:{PORT}/?complete&stream=1]]></Tracking>
 </TrackingEvents>
-<MediaFiles><MediaFile delivery="progressive" type="video/mp4" width="640" height="360">
-<![CDATA[http://{IP}:{PORT}/ad_one.mp4?stream=1]]>
-</MediaFile></MediaFiles>
+<MediaFiles>
+<MediaFile id="GDFP" delivery="progressive" width="640" height="360" type="video/mp4" bitrate="733" scalable="true" maintainAspectRatio="true">
+<![CDATA[http://{IP}:{PORT}/640x360.mp4]]>
+</MediaFile>
+</MediaFiles>
 </Linear></Creative></Creatives>
 </InLine></Ad></VAST>"""
 
@@ -67,25 +69,29 @@ def vast_stream2_custom(IP, PORT):
 <Tracking event="complete"><![CDATA[http://{IP}:{PORT}/?complete&stream=2]]></Tracking>
 </TrackingEvents>
 <MediaFiles>
-<MediaFile id="GDFP" delivery="progressive" width="1280" height="720" type="video/mp4" bitrate="3198" scalable="true" maintainAspectRatio="true">
-<![CDATA[
-http://redirector.gvt1.com/videoplayback/id/7bb3afce79f3998f/itag/22/source/gfp_video_ads/xpc/EgVovf3BOg%3D%3D/acao/yes/mime/video%2Fmp4/ctier/L/ip/0.0.0.0/ipbits/0/expire/1788378327/sparams/ip,ipbits,expire,id,itag,source,xpc,acao,mime,ctier/signature/5E4ED47794E65D6BD49E6BD4CD217577B4A4F5E7.5DED3AB4993A2B2336A567FC1CF536FF489966F3/key/ck2/file/file.mp4
-]]>
-</MediaFile>
 <MediaFile id="GDFP" delivery="progressive" width="640" height="360" type="video/mp4" bitrate="733" scalable="true" maintainAspectRatio="true">
-<![CDATA[
-http://redirector.gvt1.com/videoplayback/id/7bb3afce79f3998f/itag/18/source/gfp_video_ads/xpc/EgVovf3BOg%3D%3D/acao/yes/mime/video%2Fmp4/ctier/L/ip/0.0.0.0/ipbits/0/expire/1788378327/sparams/ip,ipbits,expire,id,itag,source,xpc,acao,mime,ctier/signature/49FFFD84F2857C3135B31553D0D89184B6A556CB.8E83D3DC5114379A24971F8CD23EAA260BD28014/key/ck2/file/file.mp4
-]]>
+<![CDATA[http://{IP}:{PORT}/640x360.mp4]]>
 </MediaFile>
-<MediaFile id="GDFP" delivery="progressive" width="1280" height="720" type="video/mp4" bitrate="3052" scalable="true" maintainAspectRatio="true">
-<![CDATA[
-http://redirector.gvt1.com/videoplayback/id/7bb3afce79f3998f/itag/106/source/gfp_video_ads/xpc/EgVovf3BOg%3D%3D/acao/yes/mime/video%2Fmp4/ctier/L/ip/0.0.0.0/ipbits/0/expire/1788378327/sparams/ip,ipbits,expire,id,itag,source,xpc,acao,mime,ctier/signature/2148264B79C438F28298855A508B8B6762EF5BE2.B203577EA406BABBA77907F6854466D6CC88474A/key/ck2/file/file.mp4
-]]>
-</MediaFile>
-<MediaFile id="GDFP" delivery="progressive" width="854" height="480" type="video/mp4" bitrate="1101" scalable="true" maintainAspectRatio="true">
-<![CDATA[
-http://redirector.gvt1.com/videoplayback/id/7bb3afce79f3998f/itag/109/source/gfp_video_ads/xpc/EgVovf3BOg%3D%3D/acao/yes/mime/video%2Fmp4/ctier/L/ip/0.0.0.0/ipbits/0/expire/1788378327/sparams/ip,ipbits,expire,id,itag,source,xpc,acao,mime,ctier/signature/8C2E69C4B9906CAE30E9FD704F30F4D4273F4C42.6516EF168813ECE43EB9666C2C7B486E4B704D7F/key/ck2/file/file.mp4
-]]>
+</MediaFiles>
+</Linear></Creative></Creatives>
+</InLine></Ad></VAST>"""
+
+def vast_stream3_custom(IP, PORT):
+    return f"""<?xml version="1.0" encoding="UTF-8"?>
+<VAST version="2.0"><Ad id="custom-beacon-test-stream3"><InLine>
+<AdSystem>local</AdSystem><AdTitle>beacon-test-stream3</AdTitle>
+<Impression><![CDATA[http://{IP}:{PORT}/?impression&stream=3]]></Impression>
+<Creatives><Creative><Linear><Duration>00:00:30</Duration>
+<TrackingEvents>
+<Tracking event="start"><![CDATA[http://{IP}:{PORT}/?start&stream=3]]></Tracking>
+<Tracking event="firstQuartile"><![CDATA[http://{IP}:{PORT}/?firstQuartile&stream=3]]></Tracking>
+<Tracking event="midpoint"><![CDATA[http://{IP}:{PORT}/?midPoint&stream=3]]></Tracking>
+<Tracking event="thirdQuartile"><![CDATA[http://{IP}:{PORT}/?thirdQuartile&stream=3]]></Tracking>
+<Tracking event="complete"><![CDATA[http://{IP}:{PORT}/?complete&stream=3]]></Tracking>
+</TrackingEvents>
+<MediaFiles>
+<MediaFile id="GDFP" delivery="progressive" width="640" height="360" type="video/mp4" bitrate="733" scalable="true" maintainAspectRatio="true">
+<![CDATA[http://{IP}:{PORT}/640x360.mp4]]>
 </MediaFile>
 </MediaFiles>
 </Linear></Creative></Creatives>
--- server.py
+++ server.py
@@ -4,13 +4,14 @@ import os
 import urllib.request
 from urllib.parse import urlparse, parse_qs
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
-from media.VAST.vast_options import vast_simple, vast_custom, vast_multiple, vast_stream2_custom, vast_stream1_custom, vast_error
+from media.VAST.vast_options import vast_simple, vast_custom, vast_multiple, vast_stream2_custom, vast_stream1_custom, vast_stream3_custom, vast_error
 
 PORT = 8082
 MEDIA_FILE = "./media/ad_one.mp4"
 MEDIA_FILES = {
     "/ad_one.mp4": "./media/ad_one.mp4",
     "/ad_two.mp4": "./media/ad_two.mp4",
+    "/640x360.mp4":"./media/640x360.mp4"
 }
 
 
@@ -59,7 +60,7 @@ def ensure_media_file(url=None):
 IP = my_ip()
 
 
-VASTS = {"simple": vast_simple, "custom": vast_custom, "multiple": vast_multiple, "stream2": vast_stream2_custom, "stream1": vast_stream1_custom, "error": vast_error}
+VASTS = {"simple": vast_simple, "custom": vast_custom, "multiple": vast_multiple, "stream2": vast_stream2_custom, "stream1": vast_stream1_custom, "stream3": vast_stream3_custom, "error": vast_error}
 
 
 class My_Server(BaseHTTPRequestHandler):
@@ -206,6 +207,7 @@ if __name__ == "__main__":
         print(f"  ad-request URL for raf.force.ad_url:  http://{IP}:{PORT}/")
         print(f"  force a specific VAST:                http://{IP}:{PORT}/custom")
         print(f"  force multiple VAST:                  http://{IP}:{PORT}/multiple")
+        print(f"  force stream3 VAST:                   http://{IP}:{PORT}/stream3")
         print(f"  force stream2 VAST:                   http://{IP}:{PORT}/stream2")
         print(f"  force stream1 VAST:                   http://{IP}:{PORT}/stream1")
         print(f"  force error VAST:                     http://{IP}:{PORT}/error")
